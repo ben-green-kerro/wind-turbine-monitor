2df86bd109e2048f645dc717b0522c2f717743fb
Ben Green <ben@kernowrobotics.co.uk>
2022-12-15 16:42:05 +0000

    updated loging

--- logging.py
+++ logging.py
@@ -1,10 +1,16 @@
-# Importing Libraries
 import serial
 import time
-arduino = serial.Serial(baudrate=115200, timeout=.1)
+import datetime
+arduino = serial.Serial(port="/dev/serial/by-id/usb-Arduino__www.arduino.cc__0043_95032303637351609161-if00", baudrate=115200, timeout=.1)
 def read():
-    data = arduino.readline()
+    data = arduino.readline().strip().decode()
     return data
+
+currenttime = datetime.datetime.now()
+hour = currenttime.hour
 while True:
-    value = read()
-    print(value)
+    while datetime.datetime.now().hour == hour:
+        if arduino.in_waiting:
+            txt = read()
+            args = txt.split(",")
+            print(args)
